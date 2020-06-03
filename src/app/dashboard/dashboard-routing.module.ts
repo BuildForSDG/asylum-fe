660e79fe88090d547762f44088c93c558a5ce09d
Letlhogonolo Theodore Obonye <ltobonye@gmail.com>
2020-06-03 10:56:50 +0200

    add view for profile

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,9 +1,13 @@
+import { ProfileComponent } from './profile/profile.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard.component';
 
-const routes: Routes = [{ path: '', component: DashboardComponent }];
+const routes: Routes = [{ path: '', component: DashboardComponent, children: [
+  {path: '', redirectTo: 'patient', pathMatch: 'full'},
+  { path: 'profile', component: ProfileComponent },
+] }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
