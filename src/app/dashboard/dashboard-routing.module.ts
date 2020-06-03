7d5c6af0c289bae9819a94e25cc5a457172817c5
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2020-06-03 17:34:44 +0300

    User should be authenticated to access dashboard

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { SpecialistsComponent } from './specialists/specialists.component';
 import { DashboardComponent } from './dashboard.component';
+import { AuthGuard } from '../shared/guards/auth.guard';
 
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
+    canActivate: [AuthGuard],
     children: [
       { path: 'account', component: ProfileComponent },
       { path: 'specialists', component: SpecialistsComponent },
