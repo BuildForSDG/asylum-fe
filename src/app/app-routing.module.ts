af6e8d9d592bc254b5bb0bde86abad7479be504b
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2020-06-03 14:09:39 +0300

    App routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,10 @@
-import { ChangePasswordComponent } from './auth/change-password/change-password.component';
-import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
-  { path: 'patient', loadChildren: () => import('./dashboard/dashboard.module').then((m) => m.DashboardModule) },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'reset', component: ResetPasswordComponent },
-  { path: 'change-password', component: ChangePasswordComponent },
-  { path: 'home', loadChildren: () => import('./home/home.module').then((m) => m.HomeModule) }
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule) },
+  { path: 'home', loadChildren: () => import('./home/home.module').then((m) => m.HomeModule) },
+  { path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
 
 @NgModule({
