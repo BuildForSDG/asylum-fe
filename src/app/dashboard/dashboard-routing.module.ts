80f1a57befdb1149667106400b8893361c9e336c
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2020-06-03 17:20:54 +0300

    Display dashboard sidebar

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -3,12 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { ProfileComponent } from './profile/profile.component';
 import { SpecialistsComponent } from './specialists/specialists.component';
+import { DashboardComponent } from './dashboard.component';
 
 
 const routes: Routes = [
-  { path: 'account', component: ProfileComponent },
-  { path: 'specialists', component: SpecialistsComponent },
-  { path: '', redirectTo: 'account', pathMatch: 'full'}
+  {
+    path: '',
+    component: DashboardComponent,
+    children: [
+      { path: 'account', component: ProfileComponent },
+      { path: 'specialists', component: SpecialistsComponent },
+      { path: '', redirectTo: 'account', pathMatch: 'full' }
+    ]
+  }
 ];
 
 @NgModule({
