198df7fca6e97f74f00885090709936cd5714943
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2020-06-03 16:17:15 +0300

    Auth module fixes

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { ProfileComponent } from './profile/profile.component';
 import { LoginComponent } from './login/login.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 
 const routes: Routes = [
-  { path: 'profile', component: ProfileComponent },
   { path: 'login', component: LoginComponent },
   { path: 'reset-password', component: ResetPasswordComponent },
-  { path: '', redirectTo: 'profile', pathMatch: 'full'}
+  { path: '', redirectTo: 'login', pathMatch: 'full'}
 ];
 
 @NgModule({
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,20 +1,28 @@
 import { RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RegisterComponent } from './register/register.component';
-import { ChangePasswordComponent } from './change-password/change-password.component';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { AuthComponent } from './auth.component';
+import { LoginComponent } from './login/login.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    AuthRoutingModule,
     FormsModule,
     ReactiveFormsModule,
     RouterModule
   ],
-  declarations: [ LoginComponent, RegisterComponent, ChangePasswordComponent, ResetPasswordComponent],
-  exports: [LoginComponent, RegisterComponent]
+  declarations: [
+    AuthComponent,
+    LoginComponent,
+    ResetPasswordComponent
+  ],
+  exports: [
+    LoginComponent
+  ]
 })
 export class AuthModule { }
