6cb5f2e71c50e9f3593787e0517ea3a71f70ec2d
Letlhogonolo Theodore Obonye <ltobonye@gmail.com>
2020-06-05 13:18:55 +0200

    revert to reset password

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ResetPasswordInitComponent } from './reset-password/init/reset-password-init.component';
+import { ResetPasswordInitComponent } from './reset-password/reset-password-init.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
