213c747a2dd93d68802eb5efa174e5bcf95f5d2a
Letlhogonolo Theodore Obonye <ltobonye@gmail.com>
2020-06-09 14:16:41 +0200

    feat: add specialist-details view for when a list is expanded to view details

--- src/app/dashboard/specialists/specialists.component.ts
+++ src/app/dashboard/specialists/specialists.component.ts
@@ -1,3 +1,4 @@
+import { SpecialistService } from './../../shared/services/specialist/specialist.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,32 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./specialists.component.scss']
 })
 export class SpecialistsComponent implements OnInit {
-users = [{
-  pk: 1,
-  username: 'admin',
-  email: 'admin@example.com',
-  first_name: 'Admin',
-  last_name: 'Root',
-  avatar: 'http://810219b18a4c.ngrok.io/api/v1/users/avatars/2020/05/Screenshot.png',
-  gender: 'male',
-  managed_account: false,
-  age: 42,
-  is_patient: true
-}, {
-  pk: 2,
-  username: 'admin',
-  email: 'admin@example.com',
-  first_name: 'Admin',
-  last_name: 'Root',
-  avatar: 'http://810219b18a4c.ngrok.io/api/v1/users/avatars/2020/05/Screenshot.png',
-  gender: 'male',
-  managed_account: false,
-  age: 42,
-  is_patient: true
-}];
-  constructor() { }
+  users: any;
+  constructor(private specialistsService: SpecialistService) { }
 
   ngOnInit(): void {
+    this.users = this.specialistsService.getUsers();
   }
 
 }
